a837311509401d7587adddbd6545a4f75d192752
Vinh <vinhpham123.np@gmail.com>
2025-07-18 02:09:38 +0700

    fix .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { loadEnv } from "vite";
 
-const env = loadEnv(process.env.NODE_ENV as string, process.cwd(), "VITE_");
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
